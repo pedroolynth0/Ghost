f7da06a9c702a8fcfd054a6a6bbb74d35e8d91c8
pedroolynth0 <pedroaifail55@hotmail.com>
2021-06-13 15:23:33 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import speech_recognition as sr 
+
+
+#Cria um reconhecedor 
+
+r = sr.Recognizer()
+
+# Abrir microfone para captura
+with sr.Microphone() as source :
+   audio =  r.listen(source) #define microfone como fonte de audio
+
+   print(r.recognize_google(audio))
