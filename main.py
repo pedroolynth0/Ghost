7fefaf7da1cabadbea6a2b5c8c8ffc649fbf9063
pedroolynth0 <pedroaifail55@hotmail.com>
2021-06-13 16:01:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
-import speech_recognition as sr 
+from vosk 
 
 
-#Cria um reconhecedor 
+ model = vosk.Model(args.model)
 
-r = sr.Recognizer()
+    if args.filename:
+        dump_fn = open(args.filename, "wb")
+    else:
+        dump_fn = None
 
-# Abrir microfone para captura
-with sr.Microphone() as source :
-   while True:
-      audio =  r.listen(source) #define microfone como fonte de audio
+    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
+                            channels=1, callback=callback):
+            print('#' * 80)
+            print('Press Ctrl+C to stop the recording')
+            print('#' * 80)
 
-      print(r.recognize_google(audio, language='pt'))
+            rec = vosk.KaldiRecognizer(model, args.samplerate)
+            while True:
+                data = q.get()
+                if rec.AcceptWaveform(data):
+                    print(rec.Result())
+                else:
+                    print(rec.PartialResult())
+                if dump_fn is not None:
+                    dump_fn.write(data)
+
+except KeyboardInterrupt:
+    print('\nDone')
+    parser.exit(0)
+except Exception as e:
+    parser.exit(type(e).__name__ + ': ' + str(e))
