c53d4799919ded1a261e966d0459450c0f473370
pedroolynth0 <pedroaifail55@hotmail.com>
2021-06-13 16:09:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,21 @@
-from vosk 
+from vosk import Model, KaldiRecognizer
+import os
+import pyaudio
 
 
- model = vosk.Model(args.model)
+model = Model("model")
+rec = KaldiRecognizer(model,16000)
+p = pyaudio.PyAudio()
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000,input=True,frames_per_buffer=8000 )
+stream.start_stream()
 
-    if args.filename:
-        dump_fn = open(args.filename, "wb")
+while True:
+    data = stream.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        print(rec.Result())
     else:
-        dump_fn = None
+        print(rec.PartialResult())
 
-    with sd.RawInputStream(samplerate=args.samplerate, blocksize = 8000, device=args.device, dtype='int16',
-                            channels=1, callback=callback):
-            print('#' * 80)
-            print('Press Ctrl+C to stop the recording')
-            print('#' * 80)
-
-            rec = vosk.KaldiRecognizer(model, args.samplerate)
-            while True:
-                data = q.get()
-                if rec.AcceptWaveform(data):
-                    print(rec.Result())
-                else:
-                    print(rec.PartialResult())
-                if dump_fn is not None:
-                    dump_fn.write(data)
-
-except KeyboardInterrupt:
-    print('\nDone')
-    parser.exit(0)
-except Exception as e:
-    parser.exit(type(e).__name__ + ': ' + str(e))
+print(rec.FinalResult())
