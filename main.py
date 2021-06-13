e1583a541eb1f096821ffa7a503b9c3364603177
pedroolynth0 <pedroaifail55@hotmail.com>
2021-06-13 17:12:18 -0300

    mudanças

--- main.py
+++ main.py
@@ -1,21 +1,43 @@
+from matplotlib.pyplot import text
 from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
+import pyttsx3
+import json
+import core
+
+#sintese de fala
+engine = pyttsx3.init()
+
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[-2].id)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
 
 
 model = Model("model")
 rec = KaldiRecognizer(model,16000)
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000,input=True,frames_per_buffer=8000 )
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000,input=True,frames_per_buffer=2048 )
 stream.start_stream()
 
 while True:
-    data = stream.read(4000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
-        print(rec.Result())
-    else:
-        print(rec.PartialResult())
+       result = rec.Result()
+       result = json.loads(result)
+       
+       if result is not None:
+          text = result['text']
 
-print(rec.FinalResult())
+          print(text)
+           
+        
+          if text == 'que horas são' or text == 'me diga as horas':
+             speak(core.SystemInfom.get_time())
+
+
